1f9cba51b029fb49caa336fc05757dcc9b04caab
NishantSambyal <nishantsambyal123@gmail.com>
2024-01-25 22:10:55 +0530

    Project Base Navigation Alias

---
+++ src/components/InputText/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextInput as Input, View } from 'react-native';
+
+const TextInput = () => {
+  return (
+    <View>
+      <Input />
+    </View>
+  );
+};
+
+export default TextInput;
